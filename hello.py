9eda5b3b4a1cbf43363c2e1b8e6182b19b7607bc
Edison <edlc@outlook.com>
2020-09-02 22:42:26 +1200

    task: Added flask app

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello flask!!!'
+
+@app.route('/login')
+def login():
+    return 'This is the login page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    print ("Username: ", username)
+    return 'User {0}'.format(username)
