d0b61b8dfad157fa776397d2813db033b1a0db39
Edison Cardenas <edlc@outlook.com>
2020-10-18 20:15:36 +1300

    chore: merge blue branch changes

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello flask!!! \n This is BLUE deployment.'
+    return 'Hello flask!!! \n This is GREEN deployment.'
 
 
 @app.route('/login')
