d6a2235376bf07b95b37fc669fef227cc4c57a39
Edison Cardenas <edlc@outlook.com>
2020-10-18 19:23:10 +1300

    chore: update blue deployment branch

--- hello.py
+++ hello.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello flask!!!'
+    return 'Hello flask!!! \n This is BLUE deployment.'
+
 
 @app.route('/login')
 def login():
     return 'NEW - This is the login page'
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -16,5 +19,6 @@ def show_user_profile(username):
     print("Username: ", username)
     return 'User {0}'.format(username)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000, debug=True) # specify port=8000
+    app.run(host='0.0.0.0', port=8000, debug=True)  # specify port=8000
