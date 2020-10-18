7a34199c39db8d50d7294195ccb138dba2638251
Edison Cardenas <edlc@outlook.com>
2020-10-18 17:53:27 +1300

    debug: creating namespace using script

--- kubernetes/create_namespace.sh
+++ kubernetes/create_namespace.sh
@@ -3,6 +3,8 @@ namespace=`kubectl get ns $1 -o name 2>/dev/null`
 if [[ -n "$namespace" ]]; then
         echo "Deleting namespace $namespace..."
         kubectl delete namespace $1
+        kubectl create namespace $1
+else 
+        kubectl create namespace $1
 fi
 
-kubectl create namespace $1
